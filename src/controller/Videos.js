5af329beabe3f0acbe3dea09fd966b24efca0b25
Martin Jurča <mjurca@centrum.cz>
2015-01-01 15:08:40 +0100

    early implementation of the Video feed section in Options (without the video feed preview)

--- src/controller/Videos.js
+++ src/controller/Videos.js
@@ -1,3 +1,19 @@
 
+import L10n from "../L10n";
+
+/**
+ * Controller for the Video feed section in the options page.
+ */
 export default class Videos {
+  /**
+   * Initializes the controller.
+   *
+   * @param $scope The angular template scope.
+   */
+  constructor($scope) {
+    $scope.options_videos_title = L10n.get("options_videos_title");
+    $scope.options_videos_showWatched = L10n.get("options_videos_showWatched");
+  }
 }
+
+Videos.$inject = ["$scope"];
