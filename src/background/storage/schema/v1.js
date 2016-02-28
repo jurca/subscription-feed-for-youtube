e49cd26e76567de0d4ffaff1bb86d1205f744c16
Martin Jurča <mjurca@centrum.cz>
2016-02-28 21:54:02 +0100

    database schema

---
+++ src/background/storage/schema/v1.js
@@ -0,0 +1,81 @@
+
+import Account from "../../../model/Account"
+import Channel from "../../../model/Channel"
+import Playlist from "../../../model/Playlist"
+import Subscription from "../../../model/Subscription"
+import Video from "../../../model/Video"
+
+export default {
+  version: 1,
+  objectStores: [{
+    name: Account.objectStore,
+    keyPath: "id",
+    autoIncrement: false,
+    indexes: []
+  }, {
+    name: Channel.objectStore,
+    keyPath: "id",
+    autoIncrement: false,
+    indexes: [{
+      name: "accountIds",
+      keyPath: "accountIds",
+      unique: false,
+      multiEntry: true
+    }, {
+      name: "incognitoSubscriptionIds",
+      keyPath: "incognitoSubscriptionIds",
+      unique: false,
+      multiEntry: true
+    }]
+  }, {
+    name: Playlist.objectStore,
+    keyPath: "id",
+    autoIncrement: false,
+    indexes: [{
+      name: "accountIds",
+      keyPath: "accountIds",
+      unique: false,
+      multiEntry: true
+    }, {
+      name: "incognitoSubscriptionIds",
+      keyPath: "incognitoSubscriptionIds",
+      unique: false,
+      multiEntry: true
+    }]
+  }, {
+    name: Subscription.objectStore,
+    keyPath: "id",
+    autoIncrement: true,
+    indexes: [{
+      name: "isIncognito",
+      keyPath: "isIncognito",
+      unique: false,
+      multiEntry: false
+    }]
+  }, {
+    name: Video.objectStore,
+    keyPath: "id",
+    autoIncrement: false,
+    indexes: [{
+      name: "publishedAt",
+      keyPath: "publishedAt",
+      unique: false,
+      multiEntry: false
+    }, {
+      name: "accountIds",
+      keyPath: "accountIds",
+      unique: false,
+      multiEntry: true
+    }, {
+      name: "incognitoSubscriptionIds",
+      keyPath: "incognitoSubscriptionIds",
+      unique: false,
+      multiEntry: true
+    }, {
+      name: "feed",
+      keyPath: ["isEnabled", "watched", "publishedAt", "id"],
+      unique: false,
+      multiEntry: false
+    }]
+  }]
+}
