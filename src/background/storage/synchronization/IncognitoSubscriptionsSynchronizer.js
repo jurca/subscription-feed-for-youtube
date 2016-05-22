1d9654a2631251276df46dece5535444250cb871
Martin Jurča <mjurca@centrum.cz>
2016-05-22 08:56:34 +0200

    dependencies update

---
+++ src/background/storage/synchronization/IncognitoSubscriptionsSynchronizer.js
@@ -0,0 +1,7 @@
+
+/**
+ * Utility for synchronizing the user's incognito subscriptions in the main
+ * data storage (Indexed DB) against the modifications reported by the Chrome's
+ * (cross-device) synchronized storage.
+ */
+export default class IncognitoSubscriptionsSynchronizer {}
